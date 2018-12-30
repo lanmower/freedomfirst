a371565944fd5864b9a412120fa0d99b00698c34
root <root@vps212761.vps.ovh.ca>
2018-12-30 06:37:15 -0500

    initial shell files set up

---
+++ genesis_start.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+DATADIR="./blockchain"
+
+if [ ! -d $DATADIR ]; then
+  mkdir -p $DATADIR;
+fi
+
+./keys.conf
+
+nodeos \
+--genesis-json $DATADIR"/../../genesis.json" \
+--signature-provider $PRIVKEY=KEY:$PUBKEY \
+--plugin eosio::producer_plugin \
+--plugin eosio::chain_api_plugin \
+--plugin eosio::http_plugin \
+--plugin eosio::history_api_plugin \
+--data-dir $DATADIR"/data" \
+--blocks-dir $DATADIR"/blocks" \
+--config-dir $DATADIR"/config" \
+--producer-name eosio \
+--http-server-address 127.0.0.1:8888 \
+--p2p-listen-endpoint 127.0.0.1:9010 \
+--access-control-allow-origin=* \
+--contracts-console \
+--http-validate-host=false \
+--verbose-http-errors \
+--enable-stale-production \
+--p2p-peer-address localhost:9011 \
+--p2p-peer-address localhost:9012 \
+--p2p-peer-address localhost:9013 \
+>> $DATADIR"/nodeos.log" 2>&1 & \
+echo $! > $DATADIR"/eosd.pid"
---
+++ hard_start.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+DATADIR="./blockchain"
+
+if [ ! -d $DATADIR ]; then
+  mkdir -p $DATADIR;
+fi
+./keys.conf
+nodeos \
+--signature-provider $PUBKEY=KEY:$PRIVKEY \
+--plugin eosio::producer_plugin \
+--plugin eosio::chain_api_plugin \
+--plugin eosio::http_plugin \
+--plugin eosio::history_api_plugin \
+--data-dir $DATADIR"/data" \
+--blocks-dir $DATADIR"/blocks" \
+--config-dir $DATADIR"/config" \
+--producer-name eosio \
+--http-server-address 127.0.0.1:8888 \
+--p2p-listen-endpoint 127.0.0.1:9010 \
+--access-control-allow-origin=* \
+--contracts-console \
+--http-validate-host=false \
+--verbose-http-errors \
+--enable-stale-production \
+--p2p-peer-address localhost:9011 \
+--p2p-peer-address localhost:9012 \
+--p2p-peer-address localhost:9013 \
+--hard-replay-blockchain \
+>> $DATADIR"/nodeos.log" 2>&1 & \
+echo $! > $DATADIR"/eosd.pid"
