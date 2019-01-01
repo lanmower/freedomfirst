48bf7eed796c85940010e7adc3bc267f18b185f8
root <root@vps212761.vps.ovh.ca>
2019-01-01 03:56:09 -0500

    to bigger server

--- start.sh
+++ start.sh
@@ -16,7 +16,7 @@ nodeos \
 --blocks-dir $DATADIR"/blocks" \
 --config-dir $DATADIR"/config" \
 --producer-name eosio \
---http-server-address 127.0.0.1:8888 \
+--http-server-address 0.0.0.0:8888 \
 --p2p-listen-endpoint 127.0.0.1:9010 \
 --access-control-allow-origin=* \
 --contracts-console \
