10e96c8498be782528de4d877eee57334f806296
lanmower <almagestfraternite@gmail.com>
2019-01-01 18:25:18 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,10 +4,12 @@ DATADIR="./blockchain"
 if [ ! -d $DATADIR ]; then
   mkdir -p $DATADIR;
 fi
-. ./keys.conf
 
+. ./keys.conf 
+echo $PUBKEY $PRIVKEY
 nodeos \
---signature-provider $PRIVKEY=KEY:$PUBKEY \
+--genesis-json $DATADIR"/../../genesis.json" \
+--signature-provider $PUBKEY=KEY:$PRIVKEY \
 --plugin eosio::producer_plugin \
 --plugin eosio::chain_api_plugin \
 --plugin eosio::http_plugin \
@@ -17,14 +19,13 @@ nodeos \
 --config-dir $DATADIR"/config" \
 --producer-name eosio \
 --http-server-address 0.0.0.0:8888 \
---p2p-listen-endpoint 127.0.0.1:9010 \
+--p2p-listen-endpoint 0.0.0.0:9010 \
 --access-control-allow-origin=* \
 --contracts-console \
 --http-validate-host=false \
 --verbose-http-errors \
 --enable-stale-production \
---p2p-peer-address localhost:9011 \
---p2p-peer-address localhost:9012 \
---p2p-peer-address localhost:9013 \
+--p2p-peer-address 54.39.23.130:9010 \
+--p2p-peer-address 192.99.154.55:9010 \
 >> $DATADIR"/nodeos.log" 2>&1 & \
 echo $! > $DATADIR"/eosd.pid"
