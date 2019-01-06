32b4dcfcd2b918bcd82fab4a0818f7106efc1da1
lan <lanmower@vps212761.vps.ovh.ca>
2019-01-06 14:03:47 -0500

    construct update

--- construct.sh
+++ construct.sh
@@ -12,48 +12,43 @@ cleos  create account eosio eosio.vpay EOS84BLRbGbFahNJEpnnJHYCoW9QPbQEk2iHsHGGS
 
 # SET UP SYSTEM CONTRACTS
 
-cleos set contract eosio.token contracts/build/eosio.token/
-cleos set contract eosio.msig contracts/build/eosio.msig/
-cleos push action eosio.token create '[ "eosio", "12000000000.0000 SYS" ]' -p eosio.token@active
-cleos push action eosio.token issue '[ "eosio", "1200000000.0000 SYS", "memo" ]' -p eosio@active
-cleos set contract eosio contracts/build/eosio.system/
+cleos set contract eosio.token build/eosio.token/
+cleos set contract eosio.msig build/eosio.msig/
+cleos push action eosio.token create '[ "eosio","1500000010.0000 SYS" ]' -p eosio.token@active
+cleos push action eosio.token issue '[ "eosio", "1500000010.0000 SYS", "memo" ]' -p eosio@active
+cleos set contract eosio build/eosio.system/
+cleos set contract eosio build/eosio.system/
 cleos push action eosio setpriv '["eosio.msig", 1]' -p eosio@active
-cleos push action eosio init '["0", "4,FF"]' -p eosio@active
+cleos push action eosio init '["0", "4,SYS"]' -p eosio@active
 
 # CREATE ACCOUNTS
 
-cleos system newaccount eosio --transfer atechshaman EOS8iK6Jq73YFrfWWebkhbfj4f3GdVokRHJAwcxJSLwz5sQb8BZba --stake-net "499999991.0000 FF" --stake-cpu "500000000.0000 FF" --buy-ram-kbytes 8192
-cleos transfer eosio atechshaman "9.0000 FF"
-cleos system newaccount eosio --transfer ali EOS8mTjKEz54sFUucizTeBSwU6PAbQRfRs8KAhrQXtwGCd1TKstpe --stake-net "499999991.0000 FF" --stake-cpu "500000000.0000 FF" --buy-ram-kbytes 8192
-cleos transfer eosio ali "9.0000 FF"
-cleos system newaccount eosio --transfer heidi EOS8StKgGD2Cda29WrUus7K73AGQVGFj9UjBy9EzL7MyuQm1z4DgE --stake-net "499999991.0000 FF" --stake-cpu "500000000.0000 FF" --buy-ram-kbytes 8192
-cleos transfer eosio heidi "9.0000 FF"
-cleos system newaccount eosio --transfer moonshine EOS8NNXqbhXwZXN5VyENG4XkSzGk8L2Pvuex6cXbmjYujzzeoqR2N --stake-net "499999991.0000 FF" --stake-cpu "500000000.0000 FF" --buy-ram-kbytes 8192
-cleos transfer eosio moonshine "9.0000 FF"
-cleos system newaccount eosio --transfer sypher EOS8BGt43Xgjv4LXmeqmQjwYoAUTEhvhxPMdXsxFx6CP8GdfnAX2c --stake-net "499999991.0000 FF" --stake-cpu "500000000.0000 FF" --buy-ram-kbytes 8192
-cleos transfer eosio sypher "9.0000 FF"
-cleos system newaccount eosio --transfer wulfen EOS7G7VKGn3gz7f7yyV9BRe9R2QwxBSnHWnazabMW3hVD4SncHMuG --stake-net "499999991.0000 FF" --stake-cpu "500000000.0000 FF" --buy-ram-kbytes 8192
-cleos transfer eosio wulfen "9.0000 FF"
-cleos system newaccount eosio --transfer stephanus EOS65uuxsziXabtoVfU8EY8ZGqfeY4NEGshjBK45PbChw2J4Gydzp --stake-net "499999991.0000 FF" --stake-cpu "500000000.0000 FF" --buy-ram-kbytes 8192
-cleos transfer eosio stephanus "9.0000 FF"
-cleos system newaccount eosio --transfer evo EOS5ap3iQbJY89hGi75tJTa2fh6iv3J2wTk5UkeHyTrJRTucBKy9e --stake-net "499999991.0000 FF" --stake-cpu "500000000.0000 FF" --buy-ram-kbytes 8192
-cleos transfer eosio evo "9.0000 FF"
-cleos system newaccount eosio --transfer koh EOS5K7EkWPhgETBkPtJ3KFB1P1HBb2yseGiYCQQzmFcT9Y8vQRURD --stake-net "499999991.0000 FF" --stake-cpu "500000000.0000 FF" --buy-ram-kbytes 8192
-cleos transfer eosio koh "9.0000 FF"
-cleos system newaccount eosio --transfer kaulukia EOS6yLUcUzV3pVxs5F1uQGKTPKmGBmJNfpeVt5HktvcQdZpQUUExG --stake-net "499999991.0000 FF" --stake-cpu "500000000.0000 FF" --buy-ram-kbytes 8192
-cleos transfer eosio occultcorner "9.0000 FF"
-cleos system newaccount eosio --transfer occultcorner EOS6yLUcUzV3pVxs5F1uQGKTPKmGBmJNfpeVt5HktvcQdZpQUUExG --stake-net "499999991.0000 FF" --stake-cpu "500000000.0000 FF" --buy-ram-kbytes 8192
-cleos transfer eosio jaydecks "9.0000 FF"
-cleos system newaccount eosio --transfer assimilation EOS71T5ZqdhoDQ7uGcJ1t5vg78pVXYGU1xpRy9GqimpqkJ8aujKXr --stake-net "499999991.0000 FF" --stake-cpu "500000000.0000 FF" --buy-ram-kbytes 8192
-cleos transfer eosio occultcorner "9.0000 FF"
-cleos system newaccount eosio --transfer supermeatboy EOS6AjN55SaQwtLZUmTN7NU6Gp98WhRMwLNaFkxcchQ87mF1Bio6U --stake-net "499999991.0000 FF" --stake-cpu "500000000.0000 FF" --buy-ram-kbytes 8192
-cleos transfer eosio occultcorner "9.0000 FF"
+cleos system newaccount eosio --transfer atechshaman EOS8iK6Jq73YFrfWWebkhbfj4f3GdVokRHJAwcxJSLwz5sQb8BZba --stake-net "50000000.0000 SYS" --stake-cpu "50000000.0000 SYS" --buy-ram-kbytes 8192
+cleos system newaccount eosio --transfer ali EOS8mTjKEz54sFUucizTeBSwU6PAbQRfRs8KAhrQXtwGCd1TKstpe --stake-net "50000000.0000 SYS" --stake-cpu "50000000.0000 SYS" --buy-ram-kbytes 8192
+cleos system newaccount eosio --transfer heidi EOS8StKgGD2Cda29WrUus7K73AGQVGFj9UjBy9EzL7MyuQm1z4DgE --stake-net "50000000.0000 SYS" --stake-cpu "50000000.0000 SYS" --buy-ram-kbytes 8192
+cleos system newaccount eosio --transfer moonshine EOS8NNXqbhXwZXN5VyENG4XkSzGk8L2Pvuex6cXbmjYujzzeoqR2N --stake-net "50000000.0000 SYS" --stake-cpu "50000000.0000 SYS" --buy-ram-kbytes 8192
+cleos system newaccount eosio --transfer sypher EOS8BGt43Xgjv4LXmeqmQjwYoAUTEhvhxPMdXsxFx6CP8GdfnAX2c --stake-net "50000000.0000 SYS" --stake-cpu "50000000.0000 SYS" --buy-ram-kbytes 8192
+cleos system newaccount eosio --transfer wulfen EOS7G7VKGn3gz7f7yyV9BRe9R2QwxBSnHWnazabMW3hVD4SncHMuG --stake-net "50000000.0000 SYS" --stake-cpu "50000000.0000 SYS" --buy-ram-kbytes 8192
+cleos system newaccount eosio --transfer stephanus EOS65uuxsziXabtoVfU8EY8ZGqfeY4NEGshjBK45PbChw2J4Gydzp --stake-net "50000000.0000 SYS" --stake-cpu "50000000.0000 SYS" --buy-ram-kbytes 8192
+cleos system newaccount eosio --transfer evo EOS5ap3iQbJY89hGi75tJTa2fh6iv3J2wTk5UkeHyTrJRTucBKy9e --stake-net "50000000.0000 SYS" --stake-cpu "50000000.0000 SYS" --buy-ram-kbytes 8192
+cleos system newaccount eosio --transfer koh EOS5K7EkWPhgETBkPtJ3KFB1P1HBb2yseGiYCQQzmFcT9Y8vQRURD --stake-net "50000000.0000 SYS" --stake-cpu "50000000.0000 SYS" --buy-ram-kbytes 8192
+cleos system newaccount eosio --transfer kaulukia EOS6yLUcUzV3pVxs5F1uQGKTPKmGBmJNfpeVt5HktvcQdZpQUUExG --stake-net "50000000.0000 SYS" --stake-cpu "50000000.0000 SYS" --buy-ram-kbytes 8192
+cleos system newaccount eosio --transfer occultcorner EOS6yLUcUzV3pVxs5F1uQGKTPKmGBmJNfpeVt5HktvcQdZpQUUExG --stake-net "50000000.0000 SYS" --stake-cpu "50000000.0000 SYS" --buy-ram-kbytes 8192
+cleos system newaccount eosio --transfer assimilation EOS71T5ZqdhoDQ7uGcJ1t5vg78pVXYGU1xpRy9GqimpqkJ8aujKXr --stake-net "50000000.0000 SYS" --stake-cpu "50000000.0000 SYS" --buy-ram-kbytes 8192
+cleos system newaccount eosio --transfer supermeatboy EOS6AjN55SaQwtLZUmTN7NU6Gp98WhRMwLNaFkxcchQ87mF1Bio6U --stake-net "50000000.0000 SYS" --stake-cpu "50000000.0000 SYS" --buy-ram-kbytes 8192
+cleos system newaccount eosio --transfer diezeldiddy EOS54unGwsB812kukffX6Vpkyn8KnVyMLjPdRoPkPzWb3PgP27Hx3 --stake-net "50000000.0000 SYS" --stake-cpu "50000000.0000 SYS" --buy-ram-kbytes 8192
+cleos system newaccount eosio --transfer jaguarpaw EOS8UA8vj3tEaBA892xo2yHhR54q24B82i6r3e4iNbgzg9F4GuxFz --stake-net "50000000.0000 SYS" --stake-cpu "50000000.0000 SYS" --buy-ram-kbytes 8192
+cleos system newaccount eosio --transfer phil EOS7UnjhXYfUnTTrpcFP9GhMF7ubcd2sSJqBNSCBii9otguUK8zTJ --stake-net "50000000.0000 SYS" --stake-cpu "50000000.0000 SYS" --buy-ram-kbytes 8192
 
 # REG PRODUCERS
 
-cleos system regproducer moonshine EOS8NNXqbhXwZXN5VyENG4XkSzGk8L2Pvuex6cXbmjYujzzeoqR2N
+cleos system regproducer moonshine EOS84tXCUNRVAjFBZCXFjfopKgNpk4jNNZnxwqCxssVbCC11NTZrq
 cleos system regproducer occultcorner EOS6xf4Z8CuAEz82iZGYHvPNiQmVqZJwBaQaZaH6sqtTypDus4M51 
 cleos system regproducer koh EOS5K7EkWPhgETBkPtJ3KFB1P1HBb2yseGiYCQQzmFcT9Y8vQRURD 
+
+cleos system voteproducer prods moonshine moonshine occultcorner koh
+cleos system voteproducer prods occultcorner moonshine occultcorner koh 
+cleos system voteproducer prods koh moonshine occultcorner koh
 
 # RESIGN
 
