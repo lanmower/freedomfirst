20abac057d14aa1aef33b2dda20c35e837182fba
lanmower <almagestfraternite@gmail.com>
2019-01-31 16:18:13 +0800

    configure fixes

--- configure.sh
+++ configure.sh
@@ -1,10 +1,10 @@
 
 #!/bin/bash
 
-read -s -p "Please specify the user name: " USER
+read -s -p "Please specify the user name: " NAME 
 echo ""
 
-if test "$USER" = ""; then
+if test "$NAME" = ""; then
         echo "$0: sorry, user name cannot be blank" >&2
         exit 1;
 fi
@@ -12,8 +12,8 @@ fi
 KEYS=`cleos create key --file keys`
 PRIVKEY=`sed -n -e '/Private key/ s/.*\: *//p' keys`
 PUBKEY=`sed -n -e '/Public key/ s/.*\: *//p' keys`
-
 echo "NAME=$NAME" > keys.conf
+chmod +x keys.conf
 echo "PRIVKEY=$PRIVKEY" >> keys.conf
 echo "PUBKEY=$PUBKEY" >> keys.conf
 echo "" >> keys.conf
