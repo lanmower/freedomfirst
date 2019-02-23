aff288cd8aa368b62294290a72ac0d9d97c08e52
lanmower <almagestfraternite@gmail.com>
2019-02-23 18:44:35 +0200

    Update install.ubuntu.18.sh

--- install.ubuntu.18.sh
+++ install.ubuntu.18.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 apt update
-wget https://github.com/eosio/eos/releases/download/v1.5.2/eosio_1.5.2-1-ubuntu-18.04_amd64.deb
+wget https://github.com/eosio/eos/releases/download/v1.6.2/eosio_1.6.2-1-ubuntu-18.04_amd64.deb
 apt install ./eosio_1.5.2-1-ubuntu-18.04_amd64.deb -y
