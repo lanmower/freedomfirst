7323a48abc21389fde54adeeadc217383e3d85a8
root <root@vps210556.vps.ovh.ca>
2019-04-18 09:03:39 -0400

    added deb

--- install.ubuntu.18.sh
+++ install.ubuntu.18.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 apt update
-wget https://github.com/eosio/eos/releases/download/v1.6.2/eosio_1.6.2-1-ubuntu-18.04_amd64.deb
-apt install ./eosio_1.5.2-1-ubuntu-18.04_amd64.deb -y
+apt install ./ff.deb -y
 cp ./logrotate.d/ff /etc/logrotate.d/ff
 
 ./firstrun.sh
